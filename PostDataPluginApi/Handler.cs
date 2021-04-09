0eab60d16d9c713143e71f2509d629eaf5c31ae1
Deng Aolin <orrin.deng@grapecity.com>
2021-04-09 10:06:50 +0800

    Init

---
+++ PostDataPluginApi/Handler.cs
@@ -0,0 +1,39 @@
+﻿using GrapeCity.Forguncy.ServerApi;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostDataPluginApi
+{
+    public class Handler : ForguncyApi
+    {
+        [Post]
+        public async Task FromPost()
+        {
+            using (var stream = new StreamReader(Context.Request.Body))
+            {
+                var content = stream.ReadToEndAsync().Result;
+
+                var bytes = Encoding.Default.GetBytes(content);
+
+                await Context.Response.Body.WriteAsync(bytes, 0, bytes.Length);
+            }
+        }
+
+        [Get]
+        public async Task FromGet()
+        {
+            var bytes = Encoding.Default.GetBytes(Context.Request.QueryString.Value);
+
+            await Context.Response.Body.WriteAsync(bytes, 0, bytes.Length);
+        }
+
+        [Post]
+        public void FromPost_Error()
+        {
+            throw new WebException("FromPost_Error throws exception.");
+        }
+    }
+}
